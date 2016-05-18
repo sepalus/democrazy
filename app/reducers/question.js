5613ce6446179937bf6a21f13d3635d4b0474876
Matts Höglund <matts.hoglund@netlight.com>
2016-05-18 13:14:40 +0300

    Fixed reducer correct update

--- app/reducers/question.js
+++ app/reducers/question.js
@@ -2,17 +2,33 @@ import { ADD_VOTE, VOTE_ADDED } from '../actions/vote';
 import { INIT_STATE } from '../actions/init';
 
 // This is empty, as data is fetched from the backend
-const initialState = {};
+const initialState = {
+	text: '',
+	candidates: [],
+	votes: []
+};
 
 function question(state = initialState, action) {
 	switch (action.type) {
 		case INIT_STATE:
-			return Object.assign({}, action.state);
+		return {
+			text: action.state.text,
+			candidates: action.state.candidates,
+			votes: [].concat(action.state.votes)
+		}
 		case VOTE_ADDED:
-			state.votes.push(action.vote);
-			return Object.assign({}, state);
+			return {
+				text: state.text,
+				candidates: state.candidates,
+				votes: [].concat(state.votes, action.vote)
+			}
 		case ADD_VOTE:
-			return Object.assign({}, state, {})
+		return {
+			text: state.text,
+			status: 'LOADING',
+			candidates: state.candidates,
+			votes: [].concat(state.votes, action.vote)
+		}
 		default:
 			return state;
 
