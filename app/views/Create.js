0c1f54159302059211c34dad4e1ecae684ca5448
Sebastian <sebastian.hognabba@gmail.com>
2016-05-19 10:49:56 +0300

    Added elements to Create.js view

--- app/views/Create.js
+++ app/views/Create.js
@@ -4,22 +4,20 @@ import Button from '../components/Button';
 import {connect} from 'react-redux';
 import actions from '../actions/candidate';
 
+require('./create.scss');
+
 class Create extends React.Component {
 
   constructor() {
       super();
       this.handleClick = this.handleClick.bind(this);
   }
+  
 
   handleClick() {
-    var candidates;
+    //var candidates;
 
-    this.serverRequest = $.getJSON("http://localhost:3000/candidate",function(json) {
-        candidates = json;
-        var newguy = {name:"mike"};
-        candidates.push(newguy);
-    });
-    this.props.dispatch(actions.addCandidate());
+    //this.props.dispatch(actions.addCandidate());
 
   }
 
@@ -27,18 +25,69 @@ class Create extends React.Component {
   render() {
 
     return (
-      <div className="question">
-			  <span class="question-title">Question</span>:
-			  <input type="text" name="question" placeholder="Who is the next American president?"/><br/>
-			  Asker:<br/>
-			  <input type="text" name="asker" placeholder="Who wants to know?"/><br/>
+      <div>
+          <div className="row results-container">
+                <div className="column small-12 title-container">
+                    <span className="heading">... </span>
+                    <h2>Create a new vote</h2>
+                </div>
+          </div>
+          <div className="row results-container">
+              <form className="create-vote-form">
+                <label>Question:</label><br/>
+                <input type="text"/><br/>
+                <label>Asker:</label><br/>
+                <input type="text"/>
+                <label>Candidates:</label><br/>
+              </form>
+          </div>
+           
 
-            <Button
-                text="Addz candidates"
-                type="nav-btn"
-                icon="fa fa-search"
-                handleClick = {this.handleClick}
-            />
+          
+          <div className="row">
+                <div className="column small-12 small-centered">
+                  <div className="add-candidate-container">
+                     <form className="add-candidate-form">
+                      <div className="add-candidate-input-wrapper">
+                        <input type="text" className="add-candidate-input" placeholder="Add name"/>
+                       </div>
+                       <div className="add-candidate-input-wrapper"> 
+                          <input type="text"  className="add-candidate-input" placeholder="Add pitch"/>
+                        </div>
+                      </form>
+                      <form className="add-candidate-form">
+                      <div className="add-candidate-input-wrapper">
+                        <input type="text" className="add-candidate-input" placeholder="Add name"/>
+                       </div>
+                       <div className="add-candidate-input-wrapper"> 
+                          <input type="text"  className="add-candidate-input" placeholder="Add pitch"/>
+                        </div>
+                      </form>
+                  </div>
+                   
+                </div>
+             
+            </div>
+
+
+            <div className="row results-container">
+              <div className="column small-12 small-centered align-center">
+                  <Button text="Add candidate"/>
+              </div>
+              <div className="column small-12 small-centered align-center">
+                  <Button text="Create vote"/>
+              </div>
+    
+          </div>
+          
+          <div>
+           
+          </div>
+
+          
+
+
+          
     </div>);
   }
 
